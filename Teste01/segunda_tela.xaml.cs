8c6b65083d7ae67d34574abcc392d21a45b7149c
João Alexandre Capellaro Almeida <joaoalexandrecapellaro@gmail.com>
2022-04-05 08:31:43 -0400

    Add Arquivos Atividade

---
+++ Teste01/segunda_tela.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Teste01
+{
+    /// <summary>
+    /// Lógica interna para segunda_tela.xaml
+    /// </summary>
+    public partial class segunda_tela : Window
+    {
+        public segunda_tela()
+        {
+            InitializeComponent();
+        }
+    }
+}
